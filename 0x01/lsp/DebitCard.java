bcd477408426637bf12080419d3af68f2d356a9e
P4NDAPOWER <79761270+larissa4lves@users.noreply.github.com>
2022-04-06 17:30:53 -0300

    Update DebitCard.java

--- 0x01/lsp/DebitCard.java
+++ 0x01/lsp/DebitCard.java
@@ -1,9 +1,10 @@
 
+
 public class DebitCard extends BankCard {
 	
 	public void validate() {
 		System.out.println("Verificando Saldo!");
-		System.out.println("Saldo disponível!");
+		System.out.println("Saldo disponÃ­vel!");
 	}
 
 }
