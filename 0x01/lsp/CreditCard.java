c3e29357db589206b870c680ea404c685eb20144
P4NDAPOWER <79761270+larissa4lves@users.noreply.github.com>
2022-04-06 17:29:51 -0300

    Update CreditCard.java

--- 0x01/lsp/CreditCard.java
+++ 0x01/lsp/CreditCard.java
@@ -1,10 +1,9 @@
 
-
 public class CreditCard extends BankCard{
 	
 	public void validate() {
 		System.out.println("Verificando Limite!");
-		System.out.println("Limite disponível!");
+		System.out.println("Limite disponÃ­vel!");
 	}
 
 }
