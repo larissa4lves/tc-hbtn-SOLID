af57a8b84813f25f0760b7445efa9a205c631aaa
P4NDAPOWER <79761270+larissa4lves@users.noreply.github.com>
2022-04-11 15:59:27 -0300

    Update Error.java

--- 0x04/error/Error.java
+++ 0x04/error/Error.java
@@ -1,7 +1,7 @@
 public class Error {
 
 
-    // Use Exceptions ao invÈs de returnar cÛdigo
+    // Use Exceptions ao inv√©s de returnar c√≥digo
     //A
     public User createUser(User user){
         try {
@@ -14,11 +14,12 @@ public class Error {
         resolveGroup(user);
         return userRepository.save(user);
     }
-
-
+   
+   
+   
     public void validate(String id){
         if(ownerRepo.count(id) == 0){
-            throw new ValidationException("id: " + id + " n„o foi informado, deveria ser informado...")
+            throw new ValidationException("id: " + id + " n√£o foi informado, deveria ser informado...")
         }
     }
 
